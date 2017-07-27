5f4bb4e8cc0ca5b4f7e81828182113d02f384743
davide <davide@davide-SVE1512H1EW>
2017-07-27 19:07:23 +0200

    Updated sql script

--- WebContent/SQL/token_table.sql
+++ WebContent/SQL/token_table.sql
@@ -3,9 +3,9 @@ CREATE DATABASE `mywebprojectdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE mywebprojectdb;
 
 CREATE TABLE `token_table` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `token` varchar(45) NOT NULL,
   `used` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
+  `base_dir` varchar(200) NOT NULL,
+  PRIMARY KEY (`token`),
   UNIQUE KEY `token_UNIQUE` (`token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
