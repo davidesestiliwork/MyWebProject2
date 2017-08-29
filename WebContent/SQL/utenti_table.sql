2b558166b02ccab58411b45fb2803c13426a3458
davide <davide.sestili@gmail.com>
2017-08-29 14:35:20 +0200

    Added utenti_table.sql

---
+++ WebContent/SQL/utenti_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE utenti  ( 
+	userName	varchar(45) NOT NULL,
+	password	varchar(150) NOT NULL,
+	base_dir	varchar(200) NOT NULL,
+	PRIMARY KEY(userName)
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+GO
